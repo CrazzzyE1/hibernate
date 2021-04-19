35cbcc334f265f5a515dd429be7ddab923ca46e2
litvakivan <litvakivan@gmail.com>
2021-04-19 15:29:16 +0300

    home task 5

--- src/main/java/com/polozov/javaspringbootlessonfour/entities/Product.java
+++ src/main/java/com/polozov/javaspringbootlessonfour/entities/Product.java
@@ -1,19 +1,32 @@
 package com.polozov.javaspringbootlessonfour.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
+import javax.persistence.*;
 import java.math.BigDecimal;
 
-@AllArgsConstructor
 @NoArgsConstructor
+@RequiredArgsConstructor
 @Getter
 @Setter
 
+@Entity
+@Table(name = "products")
 public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column
+	@NonNull
 	private String name;
+
+	@Column
+	@NonNull
 	private String description;
+
+	@Column
+	@NonNull
 	private BigDecimal price;
 }
